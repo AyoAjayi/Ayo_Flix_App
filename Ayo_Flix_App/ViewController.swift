22cd9aab8633af31b79634ceb32f7a88ec181035
Ayo <ayomide.ajayi2839@gmail.com>
2020-01-26 20:00:59 -0500

    Initial Commit

---
+++ Ayo_Flix_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ayo_Flix_App
+//
+//  Created by Ayo  on 1/26/20.
+//  Copyright © 2020 Ayo . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
